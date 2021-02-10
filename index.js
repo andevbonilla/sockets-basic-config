aaacb13919c14328c0dfa0c69b9647e8f644539c
andres bonilla <andresbonilla1256@gmail.com>
2021-02-09 23:01:30 -0500

    socket-config

---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require('express');
+
+const app = express();
+
+const http = require('http').Server(app)
+const socketIO = require('socket.io')(http, {
+    cors: {
+        origin: "http://localhost:4200",
+        methods: ["GET", "POST", "DELETE", "PUT"]
+    }
+})
+
+
+let info = "send something"
+
+socketIO.on("connection", socket => {
+
+    socket.emit("name", info)
+
+    socket.broadcast.emit("name", info)
+
+    socket.on("respuesta", resp => {
+        console.log(resp);
+    })
+
+})
+
+http.listen(3000, () => {
+    console.log("3000 connect");
+})
